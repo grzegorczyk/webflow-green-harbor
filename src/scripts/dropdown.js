b653ddf41e5471b961566d7126e8b6a5a4c576da
till.grzegorczyk <tg@brandneo.de>
2024-07-15 00:52:36 +0200

    add script

--- src/scripts/dropdown.js
+++ src/scripts/dropdown.js
@@ -1,16 +1,23 @@
 document.querySelectorAll('.leistung-dropdown-switch').forEach(function(switchElement) {
     switchElement.addEventListener('click', function() {
+        const content = switchElement.nextElementSibling;
+        const afFontElement = switchElement.querySelector('.af-font');
+
         if (switchElement.classList.contains('clicked')) {
             // Wenn "leistung-dropdown-content" nicht null ist, dann remove class "clicked"
-            const content = switchElement.nextElementSibling;
             if (content) {
                 switchElement.classList.remove('clicked');
+                if (afFontElement) {
+                    afFontElement.classList.remove('clicked');
+                }
             }
         } else {
-            // Wenn "leistung-dropdown-content" null ist, dann add class "clicked"
-            const content = switchElement.nextElementSibling;
+            // Wenn "leistung-dropdown-content" nicht null ist, dann add class "clicked"
             if (content) {
                 switchElement.classList.add('clicked');
+                if (afFontElement) {
+                    afFontElement.classList.add('clicked');
+                }
             }
         }
     });
